f4d4335d8666c671c87733c7c9abe363d063bb92
Franklin Chen <franklinchen@franklinchen.com>
2013-01-10 11:16:17 -0500

    Did not need mutable Node.

--- src/main/scala/Tree.scala
+++ src/main/scala/Tree.scala
@@ -2,10 +2,6 @@ sealed trait Tree[+A]
 
 final case object NilTree extends Tree[Nothing]
 
-/**
-  left is var only because of needing mutation to create left skewed
-  tree without stack overflow.
-  */
-case class Node[A](var left: Tree[A],
+case class Node[A](left: Tree[A],
                    v: A,
                    right: Tree[A]) extends Tree[A]
